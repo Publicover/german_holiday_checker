70785be172ae71cfea852423acdcd6f29b9a80b5
Jim Publicover <jameslpublicover@gmail.com>
2021-05-21 16:08:16 +0200

    added look-ahead support

--- app/helpers/holidays_helper.rb
+++ app/helpers/holidays_helper.rb
@@ -5,6 +5,9 @@ module HolidaysHelper
     # @day ? "YES! It's #{@day} today. Enjoy!" : "NO"
     if Holiday::HOLIDAYS.keys.include?(Time.zone.today)
       "YES! It's #{Holiday::HOLIDAYS[Time.zone.today]} today. Enjoy!"
+    elsif Holiday::HOLIDAYS.keys.include?(Time.zone.tomorrow)
+      byebug
+      "NOPE. \nBut it'll be #{Holiday::HOLIDAYS[Time.zone.tomorrow]} tomorrow."
     else
       "NOPE"
     end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_05_21_082243) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "holidays", force: :cascade do |t|
     t.string "name"
     t.date "day"
