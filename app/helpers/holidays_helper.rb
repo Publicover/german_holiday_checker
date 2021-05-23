dac0a04672b91fa5b070bfb5832de579facb74b6
Jim Publicover <jameslpublicover@gmail.com>
2021-05-23 09:54:07 +0200

    left in a byebug

--- app/helpers/holidays_helper.rb
+++ app/helpers/holidays_helper.rb
@@ -2,11 +2,9 @@
 
 module HolidaysHelper
   def holiday_text
-    # @day ? "YES! It's #{@day} today. Enjoy!" : "NO"
     if Holiday::HOLIDAYS.keys.include?(Time.zone.today)
       "YES! It's #{Holiday::HOLIDAYS[Time.zone.today]} today. Enjoy!"
     elsif Holiday::HOLIDAYS.keys.include?(Time.zone.tomorrow)
-      byebug
       "NOPE. \nBut it'll be #{Holiday::HOLIDAYS[Time.zone.tomorrow]} tomorrow."
     else
       "NOPE"
